ef2b7a19cb13966aed51ded743675f3119770cda
florenciazabala <zabalafaz@gmail.com>
2024-02-29 10:12:31 -0300

    Gateway config

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -12,41 +12,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 @SpringBootApplication
-@Controller
 public class GettingStartedApplication {
-    private final DataSource dataSource;
-
-    @Autowired
-    public GettingStartedApplication(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping("/database")
-    String database(Map<String, Object> model) {
-        try (Connection connection = dataSource.getConnection()) {
-            final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
-
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
-            final var output = new ArrayList<>();
-            while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "database";
-
-        } catch (Throwable t) {
-            model.put("message", t.getMessage());
-            return "error";
-        }
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);
